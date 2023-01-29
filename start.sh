eb180fb67a63dba93723a7ced9e4f3a560a50777
Tran Anh Pha <trananhpha2001@gmail.com>
2023-01-29 20:20:09 +0700

    update file deploy

--- start.sh
+++ start.sh
@@ -3,7 +3,8 @@
 # sync database
 echo "synchronizing db..."
 npx prisma migrate reset
-yarn prisma:migrate
+# yarn prisma:migrate
+npx prisma migrate dev
 # start server
 echo "initialize server..."
 node dist/index.js
