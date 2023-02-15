970648324787f38ede65fbf700a28b2678e3b5aa
Tran Anh Pha <trananhpha2001@gmail.com>
2023-02-15 09:59:01 +0700

    update get item by shops

--- src/services/shop.service.ts
+++ src/services/shop.service.ts
@@ -2,20 +2,33 @@ import { Prisma, Product, Shop } from "@prisma/client";
 import { context } from "../types/context.type";
 
 type FilterShops = {
+    page?: number,
+    limit?: number,
     categoryId?: number,
 }
 
 class ShopService {
     public clients = context
-    
+    public DEFAULT_PAGE = 1;
+    public DEFAULT_LIMIT = 5;
+
     public async getShops(filter?: FilterShops) {
         const condition: FilterShops = {}
+        let page = this.DEFAULT_PAGE;
+        let limit = this.DEFAULT_LIMIT;
 
+        if (filter?.page) {
+            page = filter.page;
+        }
+
+        if (filter?.limit) {
+            limit = filter.limit;
+        }
         if (filter?.categoryId) {
             condition.categoryId = Number(filter.categoryId)
         }
 
-        const shops = await this.clients.prisma.shop.findMany({where: condition})
+        const shops = await this.clients.prisma.shop.findMany({skip: page == 1 ? page - 1 : (page - 1) * limit, take: limit, where: condition})
         return shops;
     }
 
